0bd1497041ece3f881942b5e97d0859e0283467d
Irem Sen <secilreel@yahoo.com>
2019-05-08 23:11:41 -0400

    changed the login page with the updated password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -47,9 +47,11 @@ export default class LoginForm extends Component{
                     <div className="button-box">
                     <button type="submit" value="Submit">Login</button>
                     </div>
-                    <h2>For a demo, use the following:</h2>
-                    <p>username: meerkat</p>
-                    <p>password: pass1</p>
+                    <p>For a demo:
+                    <br />
+                    username: meerkat
+                    <br />
+                    password: pass1</p>
                     <div className="register">
                     <Link to={'/register'}>
                     <button type="submit" value="Submit">Register Here!</button>
